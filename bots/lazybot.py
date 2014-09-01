0a848ff951fc6e7f09f86216858069d1a6c99719
Lacy Morrow <me@lacymorrow.com>
2014-09-01 11:33:55 -0700

    Update lazybot.py

--- bots/lazybot.py
+++ bots/lazybot.py
@@ -5,9 +5,16 @@ class Robot:
         # Valid actions
         valid = []
         for loc in rg.locs_around(self.location, filter_out=('invalid', 'obstacle', 'spawn')):
-            valid = valid + ['move', loc]
+            valid = valid + [['move', loc]]
         
         next_action = valid[range(0,len(valid))]
+        
+        # if there are enemies around, move
+        for loc, bot in game['robots'].iteritems():
+            if bot.player_id != self.player_id:
+                if rg.dist(loc, self.location) <= 1:
+                    return next_action
+        
         # Move off of spawn
         if 'spawn' in rg.loc_types(sef.location):
             return next_action
